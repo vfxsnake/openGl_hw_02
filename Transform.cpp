3d229577921c96ed0c2c536c1865663d4452cb16
vfxnake <jazzjoe@gmail.com>
2023-06-25 17:04:53 -0400

    transform fucntion implemented.

--- Transform.cpp
+++ Transform.cpp
@@ -9,53 +9,87 @@
 // Helper rotation function.  Please implement this.  
 mat3 Transform::rotate(const float degrees, const vec3& axis) 
 {
-  mat3 ret;
-  // YOUR CODE FOR HW2 HERE
-  // Please implement this.  Likely the same as in HW 1.  
-  return ret;
+	// TODO: implement the Rodriguez formula here
+	vec3 n_axis = glm::normalize(axis);
+	float radians = glm::radians(degrees);
+	float cos_theta = glm::cos(radians);
+	float sin_theta = glm::sin(radians);
+	mat3 identity{vec3(1,0,0), vec3(0,1,0), vec3(0,0,1)};
+	mat3 a_dot_a_trans = { vec3(n_axis.x* n_axis.x, n_axis.x* n_axis.y, n_axis.x* n_axis.z),
+					 	  vec3(n_axis.x* n_axis.y, n_axis.y* n_axis.y, n_axis.y* n_axis.z),
+				    	  vec3(n_axis.x* n_axis.z, n_axis.y* n_axis.z, n_axis.z* n_axis.z)
+				   		};
+
+	mat3 dual_matrix = { vec3(0, -n_axis.z, n_axis.y),
+						 vec3(n_axis.z, 0, -n_axis.x),
+						 vec3(-n_axis.y, n_axis.x, 0)
+					   };
+
+	return cos_theta * identity + float(1.0 - cos_theta) * a_dot_a_trans + sin_theta * dual_matrix;
 }
 
 void Transform::left(float degrees, vec3& eye, vec3& up) 
 {
-  // YOUR CODE FOR HW2 HERE
-  // Likely the same as in HW 1.  
+  vec3 rot_vector = up;
+
+	mat3 rot_matrix = rotate(-degrees, rot_vector);
+	eye = rot_matrix * eye ;
 }
 
 void Transform::up(float degrees, vec3& eye, vec3& up) 
 {
-  // YOUR CODE FOR HW2 HERE 
-  // Likely the same as in HW 1.  
+  vec3 rot_vector = glm::normalize(glm::cross(-eye, up));
+	mat3 rot_matrix = rotate(degrees, rot_vector);
+	eye = rot_matrix * eye;
+	up = glm::normalize(rot_matrix * up);
 }
 
 mat4 Transform::lookAt(const vec3 &eye, const vec3 &center, const vec3 &up) 
 {
-  mat4 ret;
-  // YOUR CODE FOR HW2 HERE
-  // Likely the same as in HW 1.  
-  return ret;
+  vec3 z = glm::normalize(-eye);
+	vec3 x = glm::normalize(glm::cross(z, up));
+	vec3 y = glm::cross(x, z);
+
+	vec3 z_inverse = -z; 
+
+	mat4 lookAt_matrix = {
+							vec4(x.x, x.y, x.z, -glm::dot(x, eye)),
+							vec4(y.x, y.y, y.z, -glm::dot(y, eye)),
+							vec4(z_inverse.x, z_inverse.y, z_inverse.z, -glm::dot(z_inverse, eye)),
+							vec4(0, 0, 0, 1)
+						 }; 
+	return glm::transpose(lookAt_matrix);
 }
 
 mat4 Transform::perspective(float fovy, float aspect, float zNear, float zFar)
 {
-  mat4 ret;
-  // YOUR CODE FOR HW2 HERE
-  // New, to implement the perspective transform as well.  
-  return ret;
+	float tan_theta = glm::tan(fovy * 0.5);
+	float delta_far = zFar - zNear;
+
+  	mat4 ret{vec4(1 / (aspect * tan_theta), 0, 0, 0),
+  		   	 vec4(0, 1.0f / tan_theta, 0, 0),
+		   	 vec4(0, 0, -(zFar + zNear)/delta_far, (-2* zFar * zNear)/delta_far),
+		   	 vec4(0,0,-1,0)};
+
+  	return glm::transpose(ret);
 }
 
 mat4 Transform::scale(const float &sx, const float &sy, const float &sz) 
 {
-  mat4 ret;
-  // YOUR CODE FOR HW2 HERE
-  // Implement scaling 
+  mat4 ret{vec4(sx,0,0,0),
+  		   vec4(0,sy,0,0),
+		   vec4(0,0,sz,0),
+		   vec4(0,0,0, 1)};
   return ret;
 }
 
 mat4 Transform::translate(const float &tx, const float &ty, const float &tz) 
 {
-  mat4 ret;
-  // YOUR CODE FOR HW2 HERE
-  // Implement translation 
+  mat4 ret{vec4(1,0,0,0),
+  		   vec4(0,1,0,0),
+		   vec4(0,0,1,0),
+		   vec4(tx,ty,tz,1)};
+ 
   return ret;
 }
 
